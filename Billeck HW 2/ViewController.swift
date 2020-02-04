13177fdfde0b6aa92acc19f639ab5009d6d2f11d
Brendt Billeck <brendtbilleck.com>
2020-02-04 12:12:36 -0500

    Initial Commit

---
+++ Billeck HW 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Billeck HW 2
+//
+//  Created by Brendt Billeck on 2/4/20.
+//  Copyright © 2020 Brendt Billeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
